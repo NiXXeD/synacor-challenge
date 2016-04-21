bb91a81264f721aaa6f3daff99be6eb9f06f1f11
Andy Christianson <nix@nixxed.com>
2016-04-21 15:27:42 -0500

    Clean up file parsing code.

--- load.js
+++ load.js
@@ -1,6 +1,5 @@
 var fs = require('fs')
 
-
 module.exports = function() {
     var memory = parseBin('challenge.bin')
     var register = register || [0, 0, 0, 0, 0, 0, 0, 0]
@@ -9,20 +8,7 @@ module.exports = function() {
 }
 
 function parseBin(file) {
-    return fs.readFileSync(file, 'binary').trim()
-        .split('').map(i => i.charCodeAt(0)).reduce((r, v, i) => {
-        if (i % 2) {
-            var pad = "00"
-            var byte1 = v.toString(16)
-            byte1 = pad.substr(0, 2 - byte1.length) + byte1
-            var byte2 = r.past.toString(16)
-            byte2 = pad.substr(0, 2 - byte2.length) + byte2
-
-            var byte = parseInt(byte1 + byte2, 16)
-            r.ops.push(byte)
-        } else {
-            r.past = v
-        }
-        return r
-    }, {ops: []}).ops
-}
+    var stream = fs.readFileSync(file)
+    return [...Array(stream.length / 2).keys()]
+        .map(offset => stream.readUInt16LE(offset * 2))
+ }
